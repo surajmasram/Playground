20e976d9f29949abb86fa84879dade8e2d06b79c
surajmasram <50173992+surajmasram@users.noreply.github.com>
2019-07-30 22:07:35 +0530

    Test code pushed to repository.

---
+++ Sum of two digit number/Main.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Main {
+	public static void main (String[] args) {
+		// Type your code here
+      Scanner sc = new Scanner(System.in);
+      int num = sc.nextInt();
+      int sum = (num % 10) + (num / 10);
+      System.out.println(sum);
+	}
+}
